2b3f03680e4a90aefba5fd6ad191863f9aaccfb9
이지호 <cctv1998@naver.com>
2021-10-23 03:26:47 +0900

    Feat (service): createJob, readAllJob 구현

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateJobDto } from "./dto/job.dto";
+import { Job } from "./job.entity";
 
 @Injectable()
-export class JobsService {}
+export class JobsService {
+	constructor(
+		@InjectRepository(Job)
+		private jobsRepository: Repository<Job>,
+	) {}
+
+	async createJob(@Body() createJobDto: CreateJobDto): Promise<Job> {
+		// const { writer, title, detail, deadline, adress, personnel, age } =
+		// 	createJobDto;
+
+		const job = this.jobsRepository.create(createJobDto);
+		await this.jobsRepository.save(job);
+		return job;
+	}
+
+	async readAllJobs(): Promise<Job[]> {
+		return this.jobsRepository.find();
+	}
+}
