55fc0b30c19292268a46d34007c55281ad4e2d6f
이지호 <cctv1998@naver.com>
2021-10-24 03:25:46 +0900

    Feat (Job.entity) : @ApiProperty() 추가

--- src/jobs/job.entity.ts
+++ src/jobs/job.entity.ts
@@ -1,38 +1,54 @@
-import { IsUUID } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsNumber, IsString, IsUUID } from "class-validator";
 import { Base } from "src/common/entities/base.entitiy";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Job extends Base {
 	@PrimaryGeneratedColumn("uuid")
+	@ApiProperty()
 	@IsUUID()
 	id: string;
 
 	// 작성자 ID
 	@Column()
+	@ApiProperty()
+	@IsString()
 	writer: string;
 
 	// 공고 제목
 	@Column()
+	@ApiProperty()
+	@IsString()
 	title: string;
 
 	// 근무 내용
 	@Column()
+	@ApiProperty()
+	@IsString()
 	detail: string;
 
 	// 마감일
 	@Column()
+	@ApiProperty()
+	@IsDate()
 	deadline: Date;
 
 	// 인원
 	@Column()
+	@ApiProperty()
+	@IsString()
 	personnel: string;
 
 	// 희망연령
 	@Column()
+	@ApiProperty()
+	@IsNumber()
 	age: number;
 
 	// 주소
 	@Column()
+	@ApiProperty()
+	@IsString()
 	adress: string;
 }
