a7f818c187e72566a27ddb5f9e60ea5a46a6d923
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-10-28 03:51:38 +0900

    Refactor: Hashtag Controlller Post -> Get

--- src/hashtag/hashtag.controller.ts
+++ src/hashtag/hashtag.controller.ts
@@ -36,6 +36,34 @@ export class HashtagController {
 		return this.hashtagService.findAll();
 	}
 
+	@Get("search")
+	@ApiOperation({
+		summary: "해시태그 검색 API",
+		description: "제목, 카테고리 검색한다.",
+	})
+	@ApiOkResponse({ description: "성공적으로 해시태그를 가져옴" })
+	@ApiBadRequestResponse({ description: "전송된 데이터가 유효하지않음" })
+	search(@Query("name") name: string, @Query("category") category: string) {
+		let query = {};
+		if (name && category) query = { name, category };
+		if (name && !category) query = { name };
+		if (!name && category) query = { category };
+		if (!name && !category) throw new BadRequestException();
+
+		return this.hashtagService.seach(query);
+	}
+
+	@Get("category")
+	@ApiOperation({
+		summary: "모든 해시태그 검색 API",
+		description: "카테고리 목록 해시태그를 검색한다.",
+	})
+	@ApiOkResponse({ description: "성공적으로 해시태그를 가져옴" })
+	@ApiBadRequestResponse({ description: "전송된 데이터가 유효하지않음" })
+	getCategory() {
+		return this.hashtagService.findCategory();
+	}
+
 	@Post()
 	@ApiOperation({
 		summary: "해시태그 생성 API",
@@ -81,33 +109,5 @@ export class HashtagController {
 	@ApiBadRequestResponse({ description: "전송된 데이터가 유효하지않음" })
 	deleteHashtag(@Param("id") id: number) {
 		return this.hashtagService.delete(id);
-	}
-
-	@Post("category")
-	@ApiOperation({
-		summary: "해시태그 검색 API",
-		description: "카테고리 목록 해시태그를 검색한다.",
-	})
-	@ApiOkResponse({ description: "성공적으로 해시태그 목록을 가져옴" })
-	@ApiBadRequestResponse({ description: "전송된 데이터가 유효하지않음" })
-	getCategory() {
-		return this.hashtagService.findCategory();
-	}
-
-	@Post("search")
-	@ApiOperation({
-		summary: "해시태그 검색 API",
-		description: "제목, 해시태그를 검색한다.",
-	})
-	@ApiOkResponse({ description: "성공적으로 해시태그 목록을 가져옴" })
-	@ApiBadRequestResponse({ description: "전송된 데이터가 유효하지않음" })
-	search(@Query("name") name: string, @Query("category") category: string) {
-		let query = {};
-		if (name && category) query = { name, category };
-		if (name && !category) query = { name };
-		if (!name && category) query = { category };
-		if (!name && !category) throw new BadRequestException();
-
-		return this.hashtagService.seach(query);
 	}
 }
