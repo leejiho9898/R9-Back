899293cf61677fa7de73bc1fce0c702e08d3d40d
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-11-11 22:51:38 +0900

    Refactor: Review entity

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -2,13 +2,7 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsDateString, IsNumber, IsString } from "class-validator";
 import { Base } from "src/common/entities/base.entitiy";
 import { User } from "src/users/entities/user.entity";
-import {
-  Column,
-  Double,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Review extends Base {
